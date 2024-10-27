297fa5578e6161789eb459393b9adfdfa69c6455
Arvind Atmuri <atmuriarvind@gmail.com>
2024-10-27 23:12:38 +1100

    Added Calculator Display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+     <div class="calculator">
      
+     <div class="display" id="display">0</div>
+      
+      <div class="button-grid">
+
+      </div>
+
+     </div>
     </div>
   );
 }
